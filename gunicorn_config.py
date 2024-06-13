e92932c825ac3b552ec6a3c18fca9d42743a1a3f
Emmanuel Orozco <emmanuel.orozco@adeo.com>
2024-06-13 11:08:14 +0200

    updated prod port

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -11,7 +11,7 @@ threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:6001')
 
 forwarded_allow_ips = '*'
 
